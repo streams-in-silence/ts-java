ae63c6e690c0592f7e9a7d14b310f2367abbe71b
streams-in-silence <massimo.rosin@yahoo.de>
2023-10-20 23:58:17 +0200

    add more tests for is-equal

--- src/utils/is-equal.test.ts
+++ src/utils/is-equal.test.ts
@@ -40,6 +40,124 @@ describe('isEqual', () => {
       expected: false,
     },
     {
+      type: 'null',
+      a: null,
+      b: null,
+      expected: true,
+    },
+    {
+      type: 'undefined',
+      a: undefined,
+      b: undefined,
+      expected: true,
+    },
+    {
+      type: 'null and undefined',
+      a: null,
+      b: undefined,
+      expected: false,
+    },
+    {
+      type: 'Dates',
+      a: new Date(10),
+      b: new Date(10),
+      expected: true,
+    },
+    {
+      type: 'different Dates',
+      a: new Date(10),
+      b: new Date(20),
+      expected: false,
+    },
+    {
+      type: 'Maps',
+      a: new Map([
+        ['1', '1'],
+        ['2', '2'],
+      ]),
+      b: new Map([
+        ['1', '1'],
+        ['2', '2'],
+      ]),
+      expected: true,
+    },
+    {
+      type: 'different Maps',
+      a: new Map([
+        ['1', '1'],
+        ['2', '2'],
+      ]),
+      b: new Map([
+        ['1', '2'],
+        ['2', '1'],
+      ]),
+      expected: false,
+    },
+    {
+      type: 'empty Maps',
+      a: new Map(),
+      b: new Map(),
+      expected: true,
+    },
+    {
+      type: 'Sets',
+      a: new Set([1, 2, 3]),
+      b: new Set([1, 2, 3]),
+      expected: true,
+    },
+    {
+      type: 'different Sets',
+      a: new Set([1, 2, 3]),
+      b: new Set([3, 2, 1]),
+      expected: false,
+    },
+    {
+      type: 'empty Sets',
+      a: new Set(),
+      b: new Set(),
+      expected: true,
+    },
+    {
+      type: 'functions',
+      a: function test() {
+        /*empty on purpose*/
+      },
+      b: function test() {
+        /*empty on purpose*/
+      },
+      expected: true,
+    },
+    {
+      type: 'different functions',
+      a: function foo() {
+        /*empty on purpose*/
+      },
+      b: function bar() {
+        /*empty on purpose*/
+      },
+      expected: false,
+    },
+    {
+      type: 'anonymous functions',
+      a: function () {
+        /*empty on purpose*/
+      },
+      b: function () {
+        /*empty on purpose*/
+      },
+      expected: true,
+    },
+    {
+      type: 'arrow functions',
+      a: () => {
+        /*empty on purpose*/
+      },
+      b: () => {
+        /*empty on purpose*/
+      },
+      expected: true,
+    },
+    {
       type: 'arrays',
       a: [1, 2, 3],
       b: [1, 2, 3],
@@ -77,6 +195,7 @@ describe('isEqual', () => {
     },
   ])('should work for $type', ({ a, b, expected }) => {
     const result = isEqual(a, b);
+
     expect(result).toBe(expected);
   });
 });
