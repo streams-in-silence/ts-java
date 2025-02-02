974c27f4ef23ef44fd2f250eb4871d45ff3dfbb3
streams-in-silence <massimo.rosin@yahoo.de>
2025-02-02 23:07:47 +0000

    remove no longer existing workspaces for vitest

--- vitest.workspace.js
+++ vitest.workspace.js
@@ -1,9 +1,1 @@
-import { defineWorkspace } from 'vitest/config';
-
-export default defineWorkspace([
-  './packages/common/vitest.config.ts',
-  './packages/comparator/vitest.config.ts',
-  './packages/optional/vitest.config.ts',
-  './packages/typeguards/vitest.config.ts',
-  './packages/utils/vitest.config.ts',
-]);
+export default ['./packages/*/vitest.config.ts'];
