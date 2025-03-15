7b65f2eda377ce7dad3cc27435ff405a74837a37
streams-in-silence <massimo.rosin@yahoo.de>
2025-03-15 21:51:32 +0000

    feature: comparator-polyfill

--- packages/comparator/tsup.config.ts
+++ packages/comparator/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/index.ts', 'src/string.comparator.ts'],
+  entry: ['src/index.ts', 'src/string.comparator.ts', 'src/patches.ts'],
   format: ['cjs', 'esm'],
   dts: true,
   outDir: 'dist',
