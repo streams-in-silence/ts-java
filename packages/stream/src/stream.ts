c48ee269073793a734e9728c24c72027fef4d87d
streams-in-silence <massimo.rosin@yahoo.de>
2025-04-01 20:47:58 +0000

    add empty methods

--- packages/stream/src/stream.ts
+++ packages/stream/src/stream.ts
@@ -1,6 +1,28 @@
+import { Comparator } from '@ts-java/comparator';
+import type { Optional } from '@ts-java/optional';
 import type { BaseStream } from './base.stream';
 
 export abstract class Stream<T> implements BaseStream<T, Stream<T>> {
+  public static concat<T>(a: Stream<T>, b: Stream<T>): Stream<T> {
+    throw new Error('Method not implemented.');
+  }
+
+  public static empty<T>(): Stream<T> {
+    return new Stream.#Impl<T>({
+      next() {
+        return { done: true, value: undefined };
+      },
+    });
+  }
+
+  public static generate<T>(supplier: () => T): Stream<T> {
+    throw new Error('Method not implemented.');
+  }
+
+  public static iterate<T>(seed: T, f: (value: T) => T): Stream<T> {
+    throw new Error('Method not implemented.');
+  }
+
   public static of<T>(elements: T): Stream<T>;
   public static of<T>(...elements: T[]): Stream<T>;
   public static of<T>(...elements: T[]): Stream<T> {
@@ -53,6 +75,19 @@ export abstract class Stream<T> implements BaseStream<T, Stream<T>> {
     }
   };
 
+  public allMatch(predicate: (value: T) => boolean): boolean {
+    throw new Error('Method not implemented.');
+  }
+
+  public anyMatch(predicate: (value: T) => boolean): boolean {
+    throw new Error('Method not implemented.');
+  }
+
+  // @todo: add Collector class/interface
+  public collect(collector: unknown): never {
+    throw new Error('Method not implemented.');
+  }
+
   public count(): number {
     let count = 0;
 
@@ -61,6 +96,10 @@ export abstract class Stream<T> implements BaseStream<T, Stream<T>> {
     }
 
     return count;
+  }
+
+  public distinct(): Stream<T> {
+    throw new Error('Method not implemented.');
   }
 
   public filter(predicate: (value: T) => boolean): Stream<T> {
@@ -77,10 +116,35 @@ export abstract class Stream<T> implements BaseStream<T, Stream<T>> {
     return new Stream.#Impl<T>(filterIterator());
   }
 
-  public forEach(callback: (value: T) => void): void {
+  public findAny(): Optional<T> {
+    throw new Error('Method not implemented.');
+  }
+
+  public findFirst(): Optional<T> {
+    throw new Error('Method not implemented.');
+  }
+
+  public flatMap<R>(mapper: (value: T) => Stream<R>): Stream<R> {
+    throw new Error('Method not implemented.');
+  }
+
+  // @todo: replace => Stream<number> with NumberStream
+  public flatMapToNumber(mapper: (value: T) => Stream<number>): Stream<number> {
+    throw new Error('Method not implemented.');
+  }
+
+  public forEach(action: (value: T) => void): void {
     for (const elem of this.#iterable) {
-      callback(elem);
+      action(elem);
     }
+  }
+
+  public forEachOrdered(action: (value: T) => void): void {
+    throw new Error('Method not implemented.');
+  }
+
+  public limit(maxSize: number): Stream<T> {
+    throw new Error('Method not implemented.');
   }
 
   public map<U>(mapper: (element: T) => U): Stream<U> {
@@ -93,5 +157,53 @@ export abstract class Stream<T> implements BaseStream<T, Stream<T>> {
         return { value: undefined, done: true };
       },
     });
+  }
+
+  // @todo: replace Stream<number> with NumberString
+  public mapToNumber(mapper: (value: T) => number): Stream<number> {
+    throw new Error('Method not implemented.');
+  }
+
+  public max(comparator: Comparator<T>): Optional<T> {
+    throw new Error('Method not implemented.');
+  }
+
+  public min(comparator: Comparator<T>): Optional<T> {
+    throw new Error('Method not implemented.');
+  }
+
+  public noneMatch(predicate: (value: T) => boolean): boolean {
+    throw new Error('Method not implemented.');
+  }
+
+  public peek(action: (value: T) => void): Stream<T> {
+    throw new Error('Method not implemented.');
+  }
+
+  public reduce(accumulator: (value: T) => T): Optional<T>;
+  public reduce(identity: T, accumulator: (value: T) => T): T;
+  public reduce<U>(
+    identity: U,
+    accumulator: (identity: U, value: T) => U,
+    combiner: (value: U) => U
+  ): U;
+  public reduce<U>(...args: unknown[]): Optional<T> | T | U {
+    throw new Error('Method not implemented.');
+  }
+
+  public skip(n: number): Stream<T> {
+    throw new Error('Method not implemented.');
+  }
+
+  public sorted(): Stream<T>;
+  public sorted(comparator: Comparator<T>): Stream<T>;
+  public sorted(comparator?: Comparator<T>): Stream<T> {
+    throw new Error('Method not implemented.');
+  }
+
+  public toArray(): T[];
+  public toArray<A>(generator: (number: number) => A[]): A[];
+  public toArray<A>(generator?: (number: number) => A[]): T[] | A[] {
+    throw new Error('Method not implemented.');
   }
 }
