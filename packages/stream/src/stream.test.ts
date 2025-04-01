1948dac79f186b082f7e5d3e8cb5542c9909834e
streams-in-silence <massimo.rosin@yahoo.de>
2025-04-01 21:06:58 +0000

    move methods to separate describe block

--- packages/stream/src/stream.test.ts
+++ packages/stream/src/stream.test.ts
@@ -2,40 +2,42 @@ import { describe, expect, it, vitest } from 'vitest';
 import { Stream } from './stream';
 
 describe('Stream', () => {
-  describe('of', () => {
-    it('should create a new sequential Stream from a single element', () => {
-      const result = Stream.of('foo');
+  describe('static', () => {
+    describe('of', () => {
+      it('should create a new sequential Stream from a single element', () => {
+        const result = Stream.of('foo');
 
-      expect(result).toBeInstanceOf(Stream);
-      expect(result.count()).toBe(1);
+        expect(result).toBeInstanceOf(Stream);
+        expect(result.count()).toBe(1);
+      });
+
+      it('should create a sequential Stream from multiple arguments', () => {
+        const result = Stream.of('foo', 'bar', 'baz');
+
+        expect(result).toBeInstanceOf(Stream);
+        expect(result.count()).toBe(3);
+      });
+
+      it('should not unpack an array of elements into a new Stream', () => {
+        const result = Stream.of([1, 2, 3]);
+
+        expect(result).toBeInstanceOf(Stream);
+        expect(result.count()).toBe(1);
+      });
     });
 
-    it('should create a sequential Stream from multiple arguments', () => {
-      const result = Stream.of('foo', 'bar', 'baz');
+    describe('ofArray', () => {
+      it('should create a new sequential Stream from an array', () => {
+        const result = Stream.ofArray([1, 2, 3]);
 
-      expect(result).toBeInstanceOf(Stream);
-      expect(result.count()).toBe(3);
-    });
+        expect(result).toBeInstanceOf(Stream);
+      });
 
-    it('should not unpack an array of elements into a new Stream', () => {
-      const result = Stream.of([1, 2, 3]);
+      it('should create a new sequential Stream that returns each element of the provided array', () => {
+        const result = Stream.ofArray([1, 2, 3]);
 
-      expect(result).toBeInstanceOf(Stream);
-      expect(result.count()).toBe(1);
-    });
-  });
-
-  describe('ofArray', () => {
-    it('should create a new sequential Stream from an array', () => {
-      const result = Stream.ofArray([1, 2, 3]);
-
-      expect(result).toBeInstanceOf(Stream);
-    });
-
-    it('should create a new sequential Stream that returns each element of the provided array', () => {
-      const result = Stream.ofArray([1, 2, 3]);
-
-      expect(result.count()).toBe(3);
+        expect(result.count()).toBe(3);
+      });
     });
   });
 
